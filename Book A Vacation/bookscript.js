2a5704c06b0ae9754969ac3edb74ccc1a917a258
Gabrielle Ann Franco <156222595+gabfranc@users.noreply.github.com>
2024-02-01 09:39:32 -0700

    Add files via upload

---
+++ Book A Vacation/bookscript.js
@@ -0,0 +1,21 @@
+function showSidebar(){
+  const sidebar = document.querySelector('#sidebar')
+  sidebar.style.display= 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('#sidebar')
+  sidebar.style.display= 'none'
+  
+}
+
+function showSide_bar(){
+  const side_bar = document.querySelector('#side_bar')
+  side_bar.style.display= 'flex'
+}
+
+function hideSide_bar(){
+  const side_bar = document.querySelector('#side_bar')
+  side_bar.style.display= 'none'
+  
+}
